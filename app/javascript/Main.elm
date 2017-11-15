4d879755753228e75cd9125532a80cafc27b8e2b
Eric Stiens <estiens@users.noreply.github.com>
2017-11-15 00:25:21 -0600

    title change

--- app/javascript/Main.elm
+++ app/javascript/Main.elm
@@ -558,7 +558,7 @@ header : Model -> Html Msg
 header model =
     Options.styled Html.h2
       [ Typo.display4 ]
-      [ text "CR8DIGGER" ]
+      [ text "CR8DIGGEr" ]
 
 view : Model -> Html Msg
 view model =
